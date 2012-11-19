7ce7b5e0f8bba125ab0556ba02c5f4a2eca2c6a4
rodmidde <rody.middelkoop@han.nl>
2012-11-19 23:20:08 +0100

    https://github.com/rodmidde/confluence-citation-plugin/issues/1

--- src/main/java/nl/mdlware/confluence/plugins/citation/XMLDocumentWrapper.java
+++ src/main/java/nl/mdlware/confluence/plugins/citation/XMLDocumentWrapper.java
@@ -2,6 +2,6 @@ package nl.mdlware.confluence.plugins.citation;
 
 public class XMLDocumentWrapper {
     public static String wrapIntoValidXML(String pageContents) {
-        return "<?xml version=\"1.0\"?><!DOCTYPE some_name [<!ENTITY nbsp \"&#160;\">]><p>" + pageContents + "</p>";
+        return "<?xml version=\"1.0\"?><!DOCTYPE some_name [<!ENTITY nbsp \"&#160;\"><!ENTITY ndash   \"&#8211;\"><!ENTITY mdash   \"&#8212;\"><!ENTITY rsquo   \"&#8217;\">  ]><p>" + pageContents + "</p>";
     }
 }
