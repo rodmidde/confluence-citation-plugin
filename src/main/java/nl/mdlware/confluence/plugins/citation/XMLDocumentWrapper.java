0b9461d5bd08a15c6d8144e2c17687209fa7de61
Rody Middelkoop <rody.middelkoop@gmail.com>
2016-10-11 21:05:35 +0200

    ldquo escaping

--- src/main/java/nl/mdlware/confluence/plugins/citation/XMLDocumentWrapper.java
+++ src/main/java/nl/mdlware/confluence/plugins/citation/XMLDocumentWrapper.java
@@ -7,6 +7,8 @@ public class XMLDocumentWrapper {
     public String wrapIntoValidXML(String pageContents) {
         return String.format("<?xml version=\"1.0\"?><!DOCTYPE some_name [<!ENTITY nbsp \"&#160;\">" +
                 "<!ENTITY ndash   \"&#8211;\"><!ENTITY mdash   \"&#8212;\">" +
-                "<!ENTITY rsquo   \"&#8217;\"><!ENTITY lsquo   \"&#8216;\">]><p>%s</p>", pageContents);
+                "<!ENTITY rsquo   \"&#8217;\"><!ENTITY lsquo   \"&#8216;\">" +
+                "<!ENTITY ldquo   \"&#8220;\">" +
+                "]><p>%s</p>", pageContents);
     }
 }
